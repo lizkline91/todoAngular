8380b0db5e11a9fee3a6e02ee9d3054fd1359659
lizkline91 <lizkline91@gmail.com>
2014-09-02 19:51:05 -0400

    todo in angularjs
    altered code in todo to work through angular

---
+++ js/main.js
@@ -0,0 +1,34 @@
+function TodoCtrl($scope) {
+
+  $scope.todos = [
+    {text:'wash dishes', done:false, editing : false},
+    {text: 'walk dog', done:false, editing : false}
+  ];
+
+  $scope.getTotalTodos = function () {
+    return $scope.todos.length;
+  };
+
+  $scope.addTodo = function () {
+    $scope.todos.push({text:$scope.formTodoText, done:false});
+    $scope.formTodoText = '';
+  };
+
+  $scope.clearCompleted = function(){
+    $scope.todos = $scope.todos.filter(function(todo){
+      return !todo.done;
+    })
+  };
+  $scope.deleteTodo = function  (index) {
+    $scope.todos.splice(index, 1);
+};
+      
+  $scope.editTodo = function(todo){
+      todo.editing=true;
+  }
+
+  $scope.doneEditing = function(todo){
+      todo.editing=false;
+
+}
+}
